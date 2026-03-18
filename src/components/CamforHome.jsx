266c524f88e732fc192341c12f9b2a8e6641df5c
Luisa Caetano <lluisacaetanoaraujo@gmail.com>
2026-03-18 13:49:59 -0300

    Corrigir ordem logos CamforHome

--- src/components/CamforHome.jsx
+++ src/components/CamforHome.jsx
@@ -201,8 +201,8 @@ export default function CamforHome() {
 
       {/* Logos SICOOB e IFMG */}
       <div className="ch-logos-bottom">
-        <img src="/images/logo-sicoob.png" alt="SICOOB" className="ch-sicoob-bottom" />
         <img src="/images/logo-ifmg.png" alt="IFMG" className="ch-ifmg-bottom" />
+        <img src="/images/logo-sicoob.png" alt="SICOOB" className="ch-sicoob-bottom" />
       </div>
     </div>
   );
