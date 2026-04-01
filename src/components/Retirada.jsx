614f03524a69ed7ea273ddf74bce760e1f2a67bc
Luisa Caetano <lluisacaetanoaraujo@gmail.com>
2026-04-01 11:02:12 -0300

    Atualiza número do WhatsApp para recebimento de pedidos

--- src/components/Retirada.jsx
+++ src/components/Retirada.jsx
@@ -101,7 +101,7 @@ export default function Retirada({ size, onBack, onFinish, cartItems = [], isMon
 
     // Gera mensagem e link WhatsApp
     const msg = encodeURIComponent(getResumoPedidoMsg({ ...pedido }));
-    const wppLink = `https://wa.me/5537991927076?text=${msg}`;
+    const wppLink = `https://wa.me/553733220800?text=${msg}`;
     window.open(wppLink, '_blank');
 
     setShowSuccess(true);
